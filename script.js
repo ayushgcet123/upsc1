5bf5355909f13725294002cf27863ef9623c2cab
ayushgcet123 <137257078+ayushgcet123@users.noreply.github.com>
2024-11-19 00:35:59 +0530

    add new file

---
+++ script.js
@@ -0,0 +1,4 @@
+// Display a greeting message when the user visits the site
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Welcome to the UPSC & Government Jobs Information Portal!");
+});
